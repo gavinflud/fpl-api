64b21b4db7780a101266f348fc7e96b95be05e09
Gavin Flood <gavin@gavinflood.com>
2021-11-20 15:01:35 +0000

    Adds functionality to get highest and lowest scoring players filtered by stat type and position

--- src/main/kotlin/com/gavinflood/fpl/api/handlers/PlayerHandler.kt
+++ src/main/kotlin/com/gavinflood/fpl/api/handlers/PlayerHandler.kt
@@ -3,6 +3,7 @@ package com.gavinflood.fpl.api.handlers
 import com.gavinflood.fpl.api.API
 import com.gavinflood.fpl.api.domain.Player
 import com.gavinflood.fpl.api.domain.Position
+import com.gavinflood.fpl.api.domain.StatType
 
 /**
  * Exposes functions that can be called to access data related to players.
@@ -42,6 +43,119 @@ class PlayerHandler : Handler() {
         return generalInfo.elements
             .filter { playerDTO -> playerDTO.team == id }
             .map { playerDTO -> mapper.mapPlayer(playerDTO, team) }
+    }
+
+    /**
+     * Get the top scoring player(s) for a particular [statType].
+     */
+    fun getTopScoringPlayersByStatType(statType: StatType): Map<Player, Int> =
+        getPlayersWithHighestStatValue(getTotalStatValuesPerPlayer(statType))
+
+    /**
+     * Get the top scoring player(s) for a particular [statType] and [position].
+     */
+    fun getTopScoringPlayersByStatTypeAndPosition(statType: StatType, position: Position): Map<Player, Int> =
+        getPlayersWithHighestStatValue(getTotalStatValuesPerPlayer(statType)) { player -> player.position == position }
+
+    /**
+     * Get the lowest scoring player(s) for a particular [statType].
+     */
+    fun getLowestScoringPlayersByStatType(statType: StatType): Map<Player, Int> =
+        getPlayersWithLowestStatValue(getTotalStatValuesPerPlayer(statType))
+
+    /**
+     * Get the lowest scoring player(s) for a particular [statType] and [position].
+     */
+    fun getLowestScoringPlayersByStatTypeAndPosition(statType: StatType, position: Position): Map<Player, Int> =
+        getPlayersWithLowestStatValue(getTotalStatValuesPerPlayer(statType)) { player -> player.position == position }
+
+    /**
+     * Sum up the total values for a specific [statType] for each player. Returns a map with the player ID as the key
+     * and the total as the value.
+     */
+    private fun getTotalStatValuesPerPlayer(statType: StatType): Map<Int, Int> {
+        val totalsPerPlayer = mutableMapOf<Int, Int>()
+
+        val fixtures = API.fixtures.get()
+        fixtures
+            .flatMap { fixture -> fixture.stats }
+            .forEach { stat ->
+                if (stat.type == statType) {
+                    val id = stat.player.id
+
+                    if (totalsPerPlayer.containsKey(id)) {
+                        totalsPerPlayer[id] = totalsPerPlayer.getValue(id) + stat.value
+                    } else {
+                        totalsPerPlayer[id] = stat.value
+                    }
+                }
+
+            }
+
+        return totalsPerPlayer
+    }
+
+    /**
+     * Find the highest value in [totalsPerPlayer] and returns the players that match that value.
+     */
+    private fun getPlayersWithHighestStatValue(
+        totalsPerPlayer: Map<Int, Int>,
+        condition: (player: Player) -> Boolean = { true }
+    ): Map<Player, Int> {
+        var highestTotal = 0
+        val playersWithHighestTotal = mutableMapOf<Player, Int>()
+        val playerLookup = get().associateBy { player -> player.id }.filterValues(condition)
+
+        totalsPerPlayer.forEach { (playerId, goals) ->
+            val player = playerLookup[playerId]
+
+            if (player != null) {
+                if (goals > highestTotal) {
+                    playersWithHighestTotal.clear()
+                }
+
+                if (goals >= highestTotal) {
+                    highestTotal = goals
+                    playersWithHighestTotal[playerLookup.getValue(playerId)] = goals
+                }
+            }
+        }
+
+        return playersWithHighestTotal
+    }
+
+    /**
+     * Find the lowest value in [totalsPerPlayer] and returns the players that match that value.
+     */
+    private fun getPlayersWithLowestStatValue(
+        totalsPerPlayer: Map<Int, Int>,
+        condition: (player: Player) -> Boolean = { true }
+    ): Map<Player, Int> {
+        var lowestTotal = Int.MAX_VALUE
+        val playersWithLowestTotal = mutableMapOf<Player, Int>()
+        val playerLookup = get().associateBy { player -> player.id }.filterValues(condition)
+
+        totalsPerPlayer.forEach { (playerId, goals) ->
+            val player = playerLookup[playerId]
+
+            if (player != null) {
+                if (goals < lowestTotal) {
+                    playersWithLowestTotal.clear()
+                }
+
+                if (goals <= lowestTotal) {
+                    lowestTotal = goals
+                    playersWithLowestTotal[playerLookup.getValue(playerId)] = goals
+                }
+            }
+        }
+
+        // Some players may not appear in totalsPerPlayer, meaning they have 0 which is the lowest possible value
+        val playersNotInTotals = playerLookup.filterValues { player -> !totalsPerPlayer.containsKey(player.id) }
+
+        return if (playersNotInTotals.isNotEmpty())
+            playersNotInTotals.values.associateWith { 0 }
+        else playersWithLowestTotal
     }
 
 }
